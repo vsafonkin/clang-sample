d413cb941491e23d333e2ba38d043bcfc75c8e12
vsafonkin <vsafonkin@gmail.com>
2022-11-22 17:40:52 +0300

    Add hello world

---
+++ main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  sample-cli-project
+//
+//  Created by Vladimir Safonkin on 08.11.2022.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int a = 17;
+    int b = 31;
+    printf("%s\n", "Hello");
+    printf("a = %d, b = %d\n", a, b);
+    return EXIT_SUCCESS;
+}
