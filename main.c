5068c0b2b153ad2b2fb7904c97231e493eb487d3
vsafonkin <vsafonkin@gmail.com>
2022-12-12 17:47:33 +0300

    Debug

--- main.c
+++ main.c
@@ -9,7 +9,6 @@
 #include "helpers.h"
 
 #define SAY_HELLO printf("Hello!\n");
-#define SAY_GOODBYE printf("Goodbye!\n");
 #define FOR_10 for (int i = 0; i < 10; i++)
 #define FOR(n) for (int i = 0; i < n; i++)
 
