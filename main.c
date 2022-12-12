e2aaa6db60e43e85e6415d8b84cae075d3bcb534
vsafonkin <vsafonkin@gmail.com>
2022-12-12 17:44:32 +0300

    Debug

--- main.c
+++ main.c
@@ -8,8 +8,6 @@
 #include <math.h>
 #include "helpers.h"
 
-#define BEGIN_BLOCK {
-#define END_BLOCK }
 #define SAY_HELLO printf("Hello!\n");
 #define SAY_GOODBYE printf("Goodbye!\n");
 #define FOR_10 for (int i = 0; i < 10; i++)
