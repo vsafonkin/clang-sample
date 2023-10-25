5eade337138829c1b0d02b0d712baa91fbb4b827
vsafonkin <vsafonkin@gmail.com>
2023-10-25 10:27:07 +0300

    Debug

--- main.c
+++ main.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 
+int doSomething(void);
+int runSomething(void);
+int testSomething(void);
+
 int main(void)
 {
-    int firstVar = 113;
-    int secondVar = 93;
+    int firstVar = 123;
+    int secondVar = 231;
     int sum = firstVar + secondVar;
-    printf("%d\n", sum);
+    printf("sum: %d\n", sum);
+    doSomething();
     return 0;
+}
+
+int doSomething(void)
+{
+    printf("do something func\n");
+    runSomething();
+    return 123;
+}
+
+int runSomething(void) 
+{
+    printf("run something func\n");
+    testSomething();
+    return 456;
+}
+
+int testSomething()
+{
+    printf("test something func\n");
+    return 789;
 }
