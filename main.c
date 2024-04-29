61b322eba319d05265bb7cd028139ceedcb7d695
vsafonkin <vsafonkin@gmail.com>
2024-04-29 23:03:43 +0300

    Debug

--- main.c
+++ main.c
@@ -1,30 +1,12 @@
 #include <stdio.h>
 
-int doSomething(void);
-int runSomething(void);
 
 int main(void) {
-  int a = 253;
-  int b = 167;
-  doSomething();
-  int c = 123;
-  int d = 231;
-  int sum = a * b + c + d;
+  int a = 123;
+  int b = 231;
+  int c = 321;
+  int sum = a + b + c;
   printf("%d\n", sum);
   return 0;
 }
 
-int doSomething(void) {
-  int a = 113;
-  int b = 114;
-  runSomething();
-  int c = 115;
-  int d = 117;
-  int sum = a + b + c + d;
-  return sum;
-}
-
-int runSomething(void) {
-  int e = 999;
-  return e;
-}
