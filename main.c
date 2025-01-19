e6f3cc2db707270769ba89e56488a81e80aa8d4b
vsafonkin <vsafonkin@gmail.com>
2025-01-19 19:49:47 +0300

    Debug

--- main.c
+++ main.c
@@ -4,30 +4,11 @@ int main(void)
 {
   printf("============\n");
 
-  char str[] = "hello, clion";
-  printf("str length: %lu\n", sizeof(str) / sizeof(char));
-  printf("%s\n", str);
-
-  int nums[4] = {123, 456, 789, 444};
+  int nums[5] = {123, 456, 789, 444, 555};
   printf("%p\n", nums);
   printf("%p\n", nums + 1);
-  printf("%lu\n", *(&nums + 1) - nums);
-  printf("============\n");
-  printf("nums length: %lu\n", sizeof(nums) / sizeof(int));
-  printf("nums pointer: %p\n", &nums);
-  printf("nums pointer: %p\n", &nums + 1);
-
-  int const *ptr = &nums[0];
-  int const *item2 = &nums[1];
-  int const *item3 = &nums[2];
-  printf("ptr value: %p\n", ptr);
-  printf("ptr value: %p\n", item2);
-  printf("ptr value: %p\n", item3);
-  int const *n = &nums;
-  printf("n: %d\n", *n);
-
-  char *names[] = {"Alice", "Bob", "Mike"};
-  printf("%p\n", names);
+  printf("%p\n", &nums);
+  printf("%p\n", &nums + 1);
 
   return 0;
 }
