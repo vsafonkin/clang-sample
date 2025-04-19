1a56377b636ad07e1fdedfd9d9e8554f574202c3
vsafonkin <vsafonkin@gmail.com>
2025-04-19 17:56:30 +0300

    Debug

--- main.c
+++ main.c
@@ -4,14 +4,5 @@ int main(void)
 {
   printf("============\n");
 
-  int nums[5] = {123, 456, 789, 444, 555};
-  printf("%p\n", nums);
-  printf("%p\n", nums + 1);
-  printf("%p\n", &nums);
-  printf("%p\n", &nums + 1);
-  printf("============\n");
-
-  size_t const size = sizeof(nums);
-  printf("%lu\n", size);
   return 0;
 }
