ca1cb80f8fd15ddd65507def4d77b448c737b0cc
vsafonkin <vsafonkin@gmail.com>
2025-05-28 21:15:59 +0300

    Debug

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
   printf("============\n");
+
+  char *name = "Alice";
+  printf("%s\n", name);
+
+  int len = strlen(name);
+  printf("%d\n", len);
 
   return 0;
 }
