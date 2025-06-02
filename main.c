2a865a5f7773ee74a13ce586229e30dab8338f31
vsafonkin <vsafonkin@gmail.com>
2025-06-02 20:36:52 +0300

    debug

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+int addNumbers(int, int);
+
 int main(void) {
-    double a = 0.2;
-    double b = 0.1;
-    double result = a + b;
-    printf("result: %f\n", result);
+    int a = 123;
+    int b = 231;
+    int result = addNumbers(a, b);
+    printf("result: %d\n", result);
 
     return 0;
+}
+
+int addNumbers(int a, int b) {
+    int out = a + b;
+    return out;
 }
