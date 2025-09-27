10fd8cdb8633fd5b37707d95c1a8aef649ffee17
vsafonkin <vsafonkin@gmail.com>
2025-09-27 19:59:24 +0300

    Clear

--- main.c
+++ main.c
@@ -2,27 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int addNumbers(int, int);
-int randNum(int);
 
 int main(void) {
-  int a = 123;
-  int b = 231;
-  int result = addNumbers(a, b);
-  printf("result: %d\n", result);
-
-  double d, res;
-  d = 4;
-  res = sqrt(d);
-  printf("test: %f\n", res);
   printf("test: %f\n", pow(3, 4));
 
   return 0;
 }
-
-int addNumbers(int a, int b) {
-  int out = a + b;
-  return out;
-}
-
-int randNum(int limit) { return rand() % limit; }
