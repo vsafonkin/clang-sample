edea8c6aa31028e479e40fb1f1794a11991f8477
vsafonkin <vsafonkin@gmail.com>
2025-09-27 20:41:53 +0300

    Debug

--- main.c
+++ main.c
@@ -2,9 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int main(void) {
-  printf("test: %f\n", pow(3, 4));
+int main(void)
+{
+  char c;
+  c = 0x41;
+  c = 0b01000001;
+  c = 0b00111111;
+  c = 0x3f;
+  char* s;
+  s = "\u026E";
+  printf("char: %c\n", c);
+  printf("s: %c\n", *s);
 
   return 0;
 }
